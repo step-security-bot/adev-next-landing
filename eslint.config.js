a6387b6076a6f78f5ea61a66136bd5e7d16002a9
Martossy Alex <github@mail.martossy.hu>
2024-11-25 14:25:45 +0100

    Use next.config.ts

--- eslint.config.js
+++ eslint.config.js
@@ -54,7 +54,7 @@ const config = [
     },
   },
   {
-    ignores: ['**/**/node_modules', '**/**/.next', '**/**/public' /*, "env.js"*/],
+    ignores: ['**/**/node_modules', '**/**/.next', '**/**/public'],
   },
 ];
 export default config;
