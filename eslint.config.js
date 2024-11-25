757b2407d3231b0c3db88d307faa97a541c6467e
Martossy Alex <github@mail.martossy.hu>
2024-11-25 16:17:25 +0100

    Add shadcn ui

--- eslint.config.js
+++ eslint.config.js
@@ -54,7 +54,7 @@ const config = [
     },
   },
   {
-    ignores: ['**/**/node_modules', '**/**/.next', '**/**/public'],
+    ignores: ['**/**/node_modules', '**/**/.next', '**/**/public', 'src/components/ui'],
   },
 ];
 export default config;
