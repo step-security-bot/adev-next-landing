6d3ace88cacc90e1148a7a21b6685f56040871e2
Martossy Alex <github@mail.martossy.hu>
2024-11-25 16:21:40 +0100

    format

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "~/env";
-import { type NextConfig } from "next";
+import '~/env';
+import { type NextConfig } from 'next';
 
 const config: NextConfig = {};
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import { Button } from "~/components/ui/button";
+import { Button } from '~/components/ui/button';
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <Button variant={"default"} asChild={false} >Hello</Button>
+        <Button variant={'default'} asChild={false}>
+          Hello
+        </Button>
       </div>
     </main>
   );
